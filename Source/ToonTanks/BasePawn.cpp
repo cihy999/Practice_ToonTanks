9e08b808b9405af47a7e2f5dbb689d1b78363e4f
cihy999 <cihy999@gmail.com>
2022-05-14 14:00:31 +0800

    feat: 159 Applying Damage

--- Source/ToonTanks/BasePawn.cpp
+++ Source/ToonTanks/BasePawn.cpp
@@ -35,5 +35,6 @@ void ABasePawn::Fire()
 	FVector Location = ProjectileSpawnPoint->GetComponentLocation();
 	FRotator Rotation = ProjectileSpawnPoint->GetComponentRotation();
 	
-	GetWorld()->SpawnActor<AProjectile>(ProjectileClass, Location, Rotation);
+	auto Projectile = GetWorld()->SpawnActor<AProjectile>(ProjectileClass, Location, Rotation);
+	Projectile->SetOwner(this);	// Projectile 需要取得 BasePawn 的資訊
 }
