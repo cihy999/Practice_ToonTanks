986482ae5bd59e1d309f9eb9349649434fbdb6c1
cihy999 <cihy999@gmail.com>
2022-05-19 13:07:28 +0800

    feat: 163 Starting The Game

--- Source/ToonTanks/ToonTankGameMode.cpp
+++ Source/ToonTanks/ToonTankGameMode.cpp
@@ -26,6 +26,31 @@ void AToonTankGameMode::BeginPlay()
 {
 	Super::BeginPlay();
 
+	HandleGameStart();
+}
+
+void AToonTankGameMode::HandleGameStart()
+{
 	Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
 	ToonTanksPlayerController = Cast<AToonTanksPlayerController>(UGameplayStatics::GetPlayerController(this, 0));
+
+	if (ToonTanksPlayerController)
+	{
+		ToonTanksPlayerController->SetPlayerEnabledState(false);
+
+		FTimerHandle PlayerEnableTimerHandle;
+		FTimerDelegate PlayerEnableTimerDelegate = FTimerDelegate::CreateUObject
+		(
+			ToonTanksPlayerController,
+			&AToonTanksPlayerController::SetPlayerEnabledState,
+			true
+		);
+		GetWorldTimerManager().SetTimer
+		(
+			PlayerEnableTimerHandle,
+			PlayerEnableTimerDelegate,
+			StartDelay,
+			false		// ¤£­nLoop
+		);
+	}
 }
